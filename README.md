36906473b5cbece5c382bc5e23d258c9aa5c48c8
gugu <gu1230gu1230@gmail.com>
2023-07-09 22:09:40 +0800

    ElasticsearchRestTemplate 方式操作es

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. 发送QQ邮件
 3. spring bean初始化执行代码的几种方式
 4. springBoot使用Spring Data Elasticsearch Repositories操作Elasticsearch
+5. ElasticsearchRestTemplate 方式操作es
---
+++ src/test/java/com/example/demo/service/impl/ElasticsearchTemplateTest.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Commodity;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchTemplateTest {
+
+    @Autowired
+    public ElasticsearchRestTemplate elasticsearchTemplate;
+
+    @Test
+    public void testInsert() {
+        Commodity commodity = new Commodity();
+        commodity.setSkuId("1501009005");
+        commodity.setName("葡萄吐司面包（10片装）");
+        commodity.setCategory("101");
+        commodity.setPrice(160);
+        commodity.setBrand("良品铺子");
+
+        elasticsearchTemplate.save(commodity);
+    }
+
+    @Test
+    public void testQuery() {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+            .withQuery(QueryBuilders.matchQuery("name", "吐司"))
+            .build();
+        SearchHits<Commodity> search = elasticsearchTemplate.search(searchQuery, Commodity.class);
+        System.out.println(search.stream().collect(Collectors.toList()));
+    }
+
+}
