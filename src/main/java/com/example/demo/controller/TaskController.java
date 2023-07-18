ad1d4cbebe59d07763eaa3b1883b8dcd6a72b49d
gugu <gu1230gu1230@gmail.com>
2023-07-18 16:10:52 +0800

    简单定时任务，重启失效

---
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.schedule.ScheduleTask;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TaskController {
+    @Autowired
+    private ScheduleTask scheduleTask;
+
+    @GetMapping("/updateCron")
+    public String updateCron(String cron) {
+        log.info("new cron :{}", cron);
+        scheduleTask.setCron(cron);
+        return "ok";
+    }
+
+    @GetMapping("/updateTimer")
+    public String updateCron(Long timer) {
+        log.info("new timer :{}", timer);
+        scheduleTask.setTimer(timer);
+        return "ok";
+    }
+
+}
